051a9e30335cbacf5cc3a51b2470c2aad12f527b
Kristina <kachalkovakristina@mail.ru>
2023-12-05 19:21:53 +0300

    2.0.0

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -18,77 +18,120 @@ export default class GameController {
     this.playerTypes = [Bowman, Swordsman, Magician];
     this.competitorTypes = [Vampire, Undead, Daemon];
 
+    this.moveRangeAttack = {
+      Bowman: { move: 2, attackRange: 2 },
+      Swordsman: { move: 4, attackRange: 1 },
+      Magician: { move: 1, attackRange: 4 },
+      Vampire: { move: 2, attackRange: 2 },
+      Undead: { move: 4, attackRange: 1 },
+      Daemon: { move: 1, attackRange: 4 }
+    };
+
+
+    this.arrPlayers = [];
+    this.arrCompetitors = [];
+
+    this.initPlayersPositions = [];
+    this.initCompetitorsPositions = [];
+
+    this.previousIndex = -1;
+  }
+
+  initPositions(column1, column2, resultArr) {
+    for (let i = 0; i < ((this.gamePlay.boardSize * this.gamePlay.boardSize) - 1); i++) {
+      if (i % this.gamePlay.boardSize === column1 || i % this.gamePlay.boardSize === column2) {
+        resultArr.push(i);
+      }
+    }
+  }
+
+  randomInitPositions(team, initPositions, teamPositions) {
+    let positions = new Set();
+  
+    while(positions.size < team.characters.length){
+      positions.add(initPositions[Math.floor(Math.random() * initPositions.length)]);
+    }
+    let arr = Array.from(positions);
+    for (let i = 0; i < team.characters.length; i++) {
+      teamPositions.push(new PositionedCharacter(team.characters[i], arr[i]));
+    }
   }
 
   init() {
     this.gamePlay.drawUi('prairie');
 
-    const players = generateTeam(this.playerTypes, 4, 4);
-    const competitors = generateTeam(this.competitorTypes, 4, 4);
+    const teamPlayers = generateTeam(this.playerTypes, 4, 4);
+    const teamCompetitors = generateTeam(this.competitorTypes, 4, 4);
 
-    const arrPlayers = [];
-    const arrCompetitors = [];
+    this.initPositions(0, 1, this.initPlayersPositions);
 
-    let arrPlayersPositions = [];
-    let arrCompetitorsPositions = [];
+    this.initPositions((this.gamePlay.boardSize - 1), (this.gamePlay.boardSize - 2), this.initCompetitorsPositions);
 
+    this.randomInitPositions(teamPlayers, this.initPlayersPositions, this.arrPlayers);
+    this.randomInitPositions(teamCompetitors, this.initCompetitorsPositions, this.arrCompetitors);
 
-    for (let i = 0; i < ((this.gamePlay.boardSize * this.gamePlay.boardSize) - 1); i++) {
-      if (i % this.gamePlay.boardSize === 0 || i % this.gamePlay.boardSize === 1) {
-        arrPlayersPositions.push(i);
-      }
-    }
-
-    for (let i = 0; i < ((this.gamePlay.boardSize * this.gamePlay.boardSize) - 1); i++) {
-      if (i % this.gamePlay.boardSize === (this.gamePlay.boardSize - 1) || i % this.gamePlay.boardSize === (this.gamePlay.boardSize - 2)) {
-        arrCompetitorsPositions.push(i);
-      }
-    }
-
-    players.characters.forEach(character => {
-      const position = arrPlayersPositions[Math.floor(Math.random() * arrPlayersPositions.length)];
-      arrPlayersPositions = arrPlayersPositions.filter(element => !(element === position))
-      arrPlayers.push(new PositionedCharacter(character, position));
-    });
-
-    competitors.characters.forEach(character => {
-      const position = arrCompetitorsPositions[Math.floor(Math.random() * arrCompetitorsPositions.length)];
-      arrCompetitorsPositions = arrCompetitorsPositions.filter(element => !(element === position))
-      arrCompetitors.push(new PositionedCharacter(character, position));
-    });
-
-    this.gamePlay.redrawPositions([...arrPlayers, ...arrCompetitors]);
+    this.gamePlay.redrawPositions([...this.arrPlayers, ...this.arrCompetitors]);
 
     this.someMethodName();
-
-    // if (this.gamePlay.cells[1].children[0]) {
-    //   console.log('yes')
-    // }
 
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
   }
 
   someMethodName() {
-    this.gamePlay.addCellEnterListener(this.onCellEnter);
+    this.gamePlay.addCellEnterListener(this.onCellEnter.bind(this));
+    this.gamePlay.addCellLeaveListener(this.onCellLeave.bind(this));
+    this.gamePlay.addCellClickListener(this.onCellClick.bind(this));
   }
 
 
   onCellClick(index) {
-    
-    
-    // TODO: react to click
+    if (this.previousIndex != -1) {
+      this.gamePlay.deselectCell(this.previousIndex);
+    }
+
+    if (this.gamePlay.cells[index].children[0]) {
+      // console.log(this.arrPlayers)
+      let result = this.arrPlayers.find(el =>
+        this.gamePlay.cells[index].children[0].className.includes(el.character.type) &&
+        el.position === index
+      )
+      if (!result) {
+        return GamePlay.showError('Ошибка');
+      }
+      this.previousIndex = index;
+      this.gamePlay.selectCell(index);
+
+      // console.log(index)
+      this.moveCharacter(result)
+    }
   }
 
   onCellEnter(index) {
-    if (this.cells[index].children[0]) {
-      this.showCellTooltip('hello', index)
-      }
+    if (this.gamePlay.cells[index].children[0]) {
+      this.gamePlay.showCellTooltip(this.getInfoCharacter(index), index);
+      this.gamePlay.setCursor('pointer');
+    }
 
-    // TODO: react to mouse enter
+  }
+
+  moveCharacter(character) {
+    console.log(character)
+
+
+  }
+
+  getInfoCharacter(index) {
+    const commonArr = [...this.arrPlayers, ...this.arrCompetitors];
+    const positionedCharacter = commonArr.find(el => el.position === index);
+    const charact = positionedCharacter.character;
+    const message = `\u{1F396}${charact.level} \u{2694}${charact.attack} \u{1F6E1}${charact.defence} \u{2764}${charact.health}`;
+    return message;
+
   }
 
   onCellLeave(index) {
-    // TODO: react to mouse leave
+    this.gamePlay.hideCellTooltip(index);
+    this.gamePlay.setCursor('auto');
   }
 }
